999f48e22eb93a9025bcbca554ef4f30492a985e
DustinMoriarty <dustin.moriarty@protonmail.com>
2023-08-14 00:09:21 -0500

    Added tests

--- tests/test_subscriber.py
+++ tests/test_subscriber.py
@@ -1,9 +1,11 @@
+import asyncio
 from asyncio import Queue
 from unittest.mock import Mock
 
 import pytest
 
 from asyncio_signal_bus.subscriber import SignalSubscriber
+from asyncio_signal_bus.exception import SignalBusShutdownError
 
 
 @pytest.mark.asyncio
@@ -19,3 +21,17 @@ async def test_subscriber():
     async with signal_subscriber:
         await subscriber_queue.put("a")
     target_mock.assert_called_once_with("a")
+
+
+@pytest.mark.asyncio
+async def test_subscriber_timeout():
+    subscriber_queue = Queue()
+
+
+    async def foo_subscriber(signal: str):
+        await asyncio.sleep(1)
+
+    signal_subscriber = SignalSubscriber(foo_subscriber, subscriber_queue, shutdown_timeout=0.01)
+    with pytest.raises(SignalBusShutdownError):
+        async with signal_subscriber:
+            await subscriber_queue.put("a")
