fd959a48601c0548501d0d0e104c651d1ab4dd56
DustinMoriarty <dustin.moriarty@protonmail.com>
2023-08-15 15:47:05 -0500

    Queue import order

--- src/asyncio_signal_bus/publisher.py
+++ src/asyncio_signal_bus/publisher.py
@@ -3,14 +3,16 @@ from asyncio import Queue
 from typing import Awaitable, Callable, Generic, List
 
 from asyncio_signal_bus.types import R
+from asyncio_signal_bus.queue_getter import QueueGetter
 
 
 class SignalPublisher(Generic[R]):
-    def __init__(self, f: Callable[..., Awaitable[R]], queues: List[Queue]):
+    def __init__(self, f: Callable[..., Awaitable[R]], queue_getter: QueueGetter):
         self._f = f
-        self._queues = queues
+        self._queue_getter = queue_getter
 
     async def __call__(self, *args, **kwargs) -> R:
+        queues = await self._queue_getter()
         result = await self._f(*args, **kwargs)
-        await asyncio.gather(*[q.put(result) for q in self._queues])
+        await asyncio.gather(*[q.put(result) for q in queues])
         return result
--- tests/test_publisher.py
+++ tests/test_publisher.py
@@ -3,17 +3,19 @@ from asyncio import Queue
 import pytest
 
 from asyncio_signal_bus.publisher import SignalPublisher
+from asyncio_signal_bus.queue_getter import QueueGetter
 
 
 @pytest.mark.asyncio
 async def test_signal_publisher():
     async def foo_publisher(arg: str):
         return arg.upper()
+    queues = {}
+    queue_getter = QueueGetter("foo", queues)
 
-    queues = [Queue(), Queue(), Queue()]
-
-    publisher = SignalPublisher(foo_publisher, queues)
+    publisher = SignalPublisher(foo_publisher, queue_getter)
+    queues["foo"] = [Queue(), Queue(), Queue()]
 
     await publisher("a")
-    for queue in queues:
+    for queue in queues["foo"]:
         assert await queue.get() == "A"
